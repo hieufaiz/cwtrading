e5b6287c080a7acf73339e14fddd980f946f458e
Hieupn <kaiinredtie@gmail.com>
2021-03-26 20:22:53 +0700

    add delta options

--- covercall/views.py
+++ covercall/views.py
@@ -17,7 +17,7 @@ def call_option_price(S, K, t, r, sigma):
     d1 = [np.log(S / K) + (r + ((sigma**2) / 2)) * t] / (sigma * np.sqrt(t))
     d2 = d1 - sigma * np.sqrt(t)
     call_price = S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * t) *  si.norm.cdf(d2, 0.0, 1.0)
-    return call_price
+    return call_price, si.norm.cdf(d1, 0.0, 1.0)[0]
 
 def put_option_price(S, K, t, r, sigma):
     d1 = [np.log(S / K) + (r + ((sigma**2) / 2)) * t] / (sigma * np.sqrt(t))
@@ -37,12 +37,12 @@ def save_covercall(request):
             covercall.save()
             # call = mb.BS([ data.cleaned_data['assetPrice'], data.cleaned_data['strikePrice'],
             #     data.cleaned_data['rate'], data.cleaned_data['maturity'] ], data.cleaned_data['volatility']).callPrice
-            call = call_option_price( data.cleaned_data['assetPrice'], data.cleaned_data['strikePrice'],
+            call, deltaOptions = call_option_price( data.cleaned_data['assetPrice'], data.cleaned_data['strikePrice'],
                 data.cleaned_data['maturity'], data.cleaned_data['rate'], data.cleaned_data['volatility'])
             campaign = {'assetPrice': data.cleaned_data['assetPrice'], 
                 'strikePrice':  data.cleaned_data['strikePrice'], 'call': call} 
             return render(request, 'covercall/campaign.html', {'assetPrice': data.cleaned_data['assetPrice'], 
-                'strikePrice':  data.cleaned_data['strikePrice'], 'call': call[0] } )
+                'strikePrice':  data.cleaned_data['strikePrice'], 'call': call[0], 'deltaOptions': deltaOptions } )
         else:
             return HttpResponse('Bad Request')
 
