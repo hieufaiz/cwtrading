83fd177cedf92fbf854ace984a449dc86e275d6b
Hieupn <kaiinredtie@gmail.com>
2021-03-28 01:35:06 +0700

    add volatility in backtest

--- covercallbacktest/views.py
+++ covercallbacktest/views.py
@@ -4,11 +4,33 @@ from .forms import Covercallbacktest
 from .models import CoverCallBacktest
 from covercall.models import ClosePrice
 import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
 def index(request):
     backtest = Covercallbacktest 
-    closeprice = ClosePrice.objects.all()
-    return render(request, 'covercallbacktest/index.html', {'backtest': backtest, 'closeprice': closeprice})
-#def get_volatility(startDate, endDate):
+    # closeprice =  ClosePrice.objects.filter(symbol = 'FPT').values('closePrice')
+    # closeprice = closeprice.filter(date__gte = "2014-01-15", date__lte = "2014-01-20")
+    # arr_price = np.zeros(len(closeprice), )
+    # for index, price in enumerate(closeprice):
+    #     arr_price[index] = price['closePrice']
+    # s_price = pd.Series(arr_price)
+    # volatility = calculate_volatility(s_price)
+    return render(request, 'covercallbacktest/index.html', {'backtest': backtest})
+
+def calculate_volatility(data):
+    logPrice = np.log(data / data.shift(1))
+    daily_std = np.std(logPrice)
+    return daily_std.item() * 252 ** 0.5
+
+def get_volatility(startDate, endDate, symbol):
+    closeprice =  ClosePrice.objects.values(symbol = symbol).values('closePrice')
+    closeorice = closePrice.filter(date__gte = startDate, date__lte = endDate)
+    arr_price = np.zeros(len(closeprice), )
+    for index, price in enumerate(closeprice):
+        arr_price[index] = price['closePrice']
+    s_price = pd.Series(arr_price)
+    volatility = calculate_volatility(s_price)
+    return volatility
 
 
 def backtesting_covercall(request):
@@ -22,6 +44,12 @@ def backtesting_covercall(request):
         strikePrice = data.cleaned_data['strikePrice']
         if data.is_valid():
             strategy_backtester = CoverCallBacktest(symbol, startDate, endDate, callPrice, endPrice, strikePrice)
+            volatility = get_volatility(startDate, endDate, symbol)
+            breakevenPoint = callPrice + strikePrice
+            sDate = datetime.strptime(startDate, "%m/%d/%Y")
+            eDate = datetime.strptime(endDate, "%m/%d/%Y")
+            deltaTime = str(eDate - sDate)
+
             strategy_backtester.save()
         else:
             return HttpResponse('Bad Request')
